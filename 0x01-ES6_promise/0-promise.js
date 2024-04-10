e3795766428fe9cdb56fc7529366db9b79b0bf3c
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-04-10 13:13:55 +0200

    promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+  /* eslint-disable no-unused-vars */
+  return new Promise((resolve, reject) => {
+    resolve();
+  });
+  /* eslint-enable no-unused-vars */
+}
