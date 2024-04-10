346b95a418533478681b1339cc420a24dbb546bc
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-04-10 19:15:15 +0200

    then

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+export default function handleResponseFromAPI(promise) {
+  /* eslint-disable no-unused-vars */
+  promise
+    .then((response) => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch((error) => {
+      console.error('Got an error from the API');
+      return new Error();
+    });
+}
