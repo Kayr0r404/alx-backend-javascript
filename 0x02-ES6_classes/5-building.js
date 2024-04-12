4580ebaae128e6ecdac1e3beb564e041e5a20cb0
Karabo Mahlare <123119832+Kayr0r404@users.noreply.github.com>
2024-04-12 15:27:27 +0200

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,31 +1,16 @@
 export default class Building {
-    /* eslint-disable class-methods-use-this */
-    /* eslint-disable no-underscore-dangle */
-    constructor(sqft) {
-      if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error(
-          'Class extending Building must override evacuationWarningMessage',
-        );
-      }}
-      if (typeof sqft !== 'number') {
-        throw new TypeError('sqft must be a number');
-      }
-      this._sqft = sqft;
-    }
-  
-    set sqft(sqft) {
-      if (typeof sqft !== 'number') {
-        throw new TypeError('sqft must be a number');
-      }
-      this._sqft = sqft;
-    }
-  
-    get sqft() {
-      return this._sqft;
-    }
-    evacuationWarningMessage() {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
-  
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
