6a7127e5847b7dd6ddc85d95fb3998b188ffe861
Karabo Mahlare <123119832+Kayr0r404@users.noreply.github.com>
2024-04-12 15:10:58 +0200

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -21,8 +21,5 @@ export default class Building {
     get sqft() {
       return this._sqft;
     }
-    evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
   }
   
