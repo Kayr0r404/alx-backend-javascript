f2b37db7d8de4e8280651ec6ee45ea8dbc2c96b4
Karabo Mahlare <123119832+Kayr0r404@users.noreply.github.com>
2024-04-12 15:25:17 +0200

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -7,7 +7,7 @@ export default class Building {
         throw new Error(
           'Class extending Building must override evacuationWarningMessage',
         );
-      }
+      }}
       if (typeof sqft !== 'number') {
         throw new TypeError('sqft must be a number');
       }
