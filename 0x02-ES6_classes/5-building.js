cfb31d6469e7276b06b0209d55d3ef11be365569
Karabo Mahlare <123119832+Kayr0r404@users.noreply.github.com>
2024-04-12 15:07:25 +0200

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,21 +1,28 @@
 export default class Building {
-  /* eslint-disable class-methods-use-this */
-  /* eslint-disable no-underscore-dangle */
-  constructor(sqft) {
-    if (typeof sqft !== 'number') throw new TypeError('sqft must be a number');
-    this._sqft = sqft;
-  }
-
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') throw new TypeError('sqft must be a number');
-    this._sqft = sqft;
-  }
-
-  get sqft() {
-    return this._sqft;
-  }
-
+    /* eslint-disable class-methods-use-this */
+    /* eslint-disable no-underscore-dangle */
+    constructor(sqft) {
+      if (this.evacuationWarningMessage === undefined) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+      if (typeof sqft !== 'number') {
+        throw new TypeError('sqft must be a number');
+      }
+      this._sqft = sqft;
+    }
+  
+    set sqft(sqft) {
+      if (typeof sqft !== 'number') {
+        throw new TypeError('sqft must be a number');
+      }
+      this._sqft = sqft;
+    }
+  
+    get sqft() {
+      return this._sqft;
+    }
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
-}
+  }
+  
