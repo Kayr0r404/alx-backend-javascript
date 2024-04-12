1d8807d97a4323bf5f3aad0e33192d70d60521b2
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-04-12 08:49:33 +0200

    classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,32 @@
+export default class HolbertonCourse {
+  /* eslint-disable no-underscore-dangle */
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  set length(length) {
+    this._length = length;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
