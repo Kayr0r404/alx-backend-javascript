ec84443e3d9f6eca092e2525145a8a0850b44d6e
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-04-20 04:07:36 +0200

    typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,18 +1,10 @@
-function createInt8TypedArray(length, position, size) {
-  if (position < 0 || position >= length) {
+function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
     throw new Error('Position outside range');
   }
-
-  // Create a new ArrayBuffer of the specified length
-  const buffer = new ArrayBuffer(length);
-
-  // Create a view of the buffer as Int8Array
-  const int8Array = new Int8Array(buffer);
-
-  // Set the value at the specified position
-  int8Array[position] = size;
-
-  return buffer;
+  const buf = new DataView(new ArrayBuffer(length), 0, length);
+  buf.setInt8(position, value);
+  return buf;
 }
 
 export default createInt8TypedArray;
