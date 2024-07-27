11c5d9071832582f421902fa4901a81f96c7cd4c
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-07-27 03:21:54 +0200

    stubbing

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,24 +4,19 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-    let calculateNumberStub;
-    let consoleSpy;
-
-    beforeEach(() => {
-        calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-        consoleSpy = sinon.spy(console, 'log');
-    });
-
-    afterEach(() => {
-        calculateNumberStub.restore();
-        consoleSpy.restore();
-    });
 
     it('should call Utils.calculateNumber with "SUM", 100, 20 and log the correct message', () => {
-        sendPaymentRequestToApi(100, 20);
+        const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+        const consoleSpy = sinon.spy(console, 'log');
         
+        sendPaymentRequestToApi(100, 20);
+            
         expect(calculateNumberStub.calledOnce).to.be.true;
         expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
         expect(consoleSpy.calledOnceWith('The total is: 10')).to.be.true;
+
+        calculateNumberStub.restore();
+        consoleSpy.restore();
     });
-});
+    });
+
