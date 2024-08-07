e7cd84f9325091ff8d6d84e8d54c6afec7be736b
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-08-07 21:48:17 +0200

    consoole

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+
+/**
+ * display string on the STDOUT
+ * @param {*} str string parameter to be printed stdout
+ * @author Kaycodes
+ */
+function displayMessage(str) {
+    console.log(str);
+}
+module.exports = displayMessage;
