89943742aa17d7a967216fa71a8c3f99bff32e2f
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-08-09 20:14:01 +0200

    http server in node

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,12 +4,12 @@ const port = 1245;
 
 const app = http.createServer((request, response) => {
   response.statusCode = 200;
-  response.setHeader('Content-Type': 'text/plain');
+  response.setHeader('Content-Type', 'text/plain');
   response.end('Hello Holberton School!');
 });
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`server listening on port ${port}`);
 });
 
 module.exports = app;
