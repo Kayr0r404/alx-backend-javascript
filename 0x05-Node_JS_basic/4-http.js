601f398eb3e5f2215298428749d548ee4b099d86
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-08-09 20:02:54 +0200

    endpoint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,8 +4,8 @@ const port = 1245;
 
 const app = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write('Hello Holberton School!');
-  response.end();
+//   response.write();
+  response.end('Hello Holberton School!');
 }).listen(port);
 
 module.exports = app;
