a78d597073a14621288821969aa7156b6f17be0d
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-08-09 09:32:01 +0200

    eslint compliant

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,11 @@
-
 /**
  * display string on the STDOUT
- * @param {*} str string parameter to be printed stdout
+ *
+ * @param {string} str string parameter to be printed stdout
  * @author Kaycodes
  */
 function displayMessage(str) {
-    console.log(str);
+  console.log(str);
 }
+
 module.exports = displayMessage;
