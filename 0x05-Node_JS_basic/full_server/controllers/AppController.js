0ed06ee2d8751e833b695e59f10edee9da2f1a12
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-08-27 04:05:15 +0200

    full

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++
@@ -1,9 +0,0 @@
-
-
-class AppController {
-    static getHomepage(request, response) {
-        response.statusCode = 200;
-        response.end('Hello Holberton School!');
-    }
-}
-export default AppController;
